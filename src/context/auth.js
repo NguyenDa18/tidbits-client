41bfc89a24a8e11456615a4f021d58d460cc8b1d
Danh Nguyen <danhnguyen267@gmail.com>
2020-04-05 20:09:13 -0700

    Adding frontend components and init npm installs

---
+++ src/context/auth.js
@@ -0,0 +1,23 @@
+import React, { useReducer, createContext } from 'react'
+import jwtDecode from 'jwt-decode'
+
+const initialState = {
+    user: null
+}
+
+if (localStorage.getItem('jwtToken')) {
+    const decodedToken = jwtDecode(localStorage.getItem('jwtToken'))
+
+    if (decodedToken.exp * 1000 < Date.now()) {
+        localStorage.removeItem('jwtToken')
+    }
+    else {
+        initialState.user = decodedToken
+    }
+}
+
+const AuthContext = createContext({
+    user: null,
+    login: (userData) => { },
+    logout: () => { }
+})
