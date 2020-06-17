17bd2a20cc8f1d7bcffa21145674e5f0c4b1e64f
Danh Nguyen <danhnguyen267@gmail.com>
2020-06-16 23:25:21 -0700

    Adding Login component and hooked auth context

--- src/App.js
+++ src/App.js
@@ -5,18 +5,25 @@ import { Container } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 
+import { AuthProvider } from './context/auth'
+import AuthRoute from './components/AuthRoute'
+
 import MenuBar from './components/MenuBar'
 import Home from './pages/Home'
+import Login from './pages/Login'
 import Register from './pages/Register'
 
 const App = () => (
-  <Router>
-    <Container>
-      <MenuBar />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/register" component={Register} />
-    </Container>
-  </Router>
+  <AuthProvider>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <AuthRoute exact path="/login" component={Login} />
+        <AuthRoute exact path="/register" component={Register} />
+      </Container>
+    </Router>
+  </AuthProvider>
 )
 
 export default App;
