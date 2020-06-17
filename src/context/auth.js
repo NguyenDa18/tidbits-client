87975bd1dbd7ceaded4c12016385f2f07bb350ce
Danh Nguyen <danhnguyen267@gmail.com>
2020-06-16 19:07:11 -0700

    Displaying user posts on home page

--- src/context/auth.js
+++ src/context/auth.js
@@ -21,3 +21,46 @@ const AuthContext = createContext({
     login: (userData) => { },
     logout: () => { }
 })
+
+function authReducer(state, action) {
+    switch (action.type) {
+        case 'LOGIN':
+            return {
+                ...state,
+                user: action.payload
+            };
+        case 'LOGOUT':
+            return {
+                ...state,
+                user: null
+            };
+        default:
+            return state;
+    }
+}
+
+const AuthProvider = (props) => {
+    const [state, dispatch] = useReducer(authReducer, initialState);
+
+    function login(userData) {
+        localStorage.setItem('jwtToken', userData.token);
+        dispatch({
+            type: 'LOGIN',
+            payload: userData
+        });
+    }
+
+    function logout() {
+        localStorage.removeItem('jwtToken');
+        dispatch({ type: 'LOGOUT' });
+    }
+
+    return (
+        <AuthContext.Provider
+            value={{ user: state.user, login, logout }}
+            {...props}
+        />
+    );
+}
+
+export { AuthContext, AuthProvider };
